04d46fb829c118b73bf1afa9afc4ad665e3dadc4
empiredesign1510-pixel <empiredesign1510@gmail.com>
2026-09-20 20:11:10 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='invoiceku-v6.1-business-suite';
+const CACHE='invoiceku-v6.2-support-author';
 const LOCAL=[
   './','./index.html','./styles.css','./manifest.webmanifest',
   './assets/brand-mark.png','./assets/icon-192.png','./assets/icon-512.png',
